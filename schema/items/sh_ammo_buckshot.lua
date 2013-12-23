2e3f9f3d8b1bbf6ad5c2d32832120bcb068b0ba8
Green3 <afik.bracha@yahoo.com>
2013-12-23 23:23:55 +0200

    Added flag

--- schema/items/sh_ammo_buckshot.lua
+++ schema/items/sh_ammo_buckshot.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -8,6 +8,7 @@ local ITEM = Clockwork.item:New("ammo_base");
 	ITEM.cost = 210;
 	ITEM.model = "models/items/boxbuckshot.mdl";
 	ITEM.weight = 1;
+	ITEM.access = "V";
 	ITEM.uniqueID = "ammo_buckshot";
 	ITEM.business = true;
 	ITEM.ammoClass = "buckshot";
