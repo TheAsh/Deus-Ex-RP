4d8bd3ebfbfcd499d0adadf1100c799ac49862bb
Green3 <afik.bracha@yahoo.com>
2013-12-23 19:55:01 +0200

    Update and rename sh_testsubject.lua to sh_civillian.lua

--- schema/classes/sh_civillian.lua
+++ schema/classes/sh_civillian.lua
@@ -1,14 +1,14 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
-local CLASS = Clockwork.class:New("Test Subject");
+local CLASS = Clockwork.class:New("Civilian");
 	CLASS.wages = 50;
 	CLASS.color = Color(150, 125, 100, 255);
 	CLASS.factions = {FACTION_CIVILIAN};
 	CLASS.isDefault = true;
 	CLASS.wagesName = "Supplies";
-	CLASS.description = "The test subject of an Aperture Science experiment.";
+	CLASS.description = "The casual civilian within the city.";
 	CLASS.defaultPhysDesc = "Wearing tattered clothing";
-CLASS_TESTSUBJECT = CLASS:Register();
+CLASS_CIVILIAN = CLASS:Register();
