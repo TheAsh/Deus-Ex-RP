daebbdc3ea26d0c1af6d82a1c5cd1a90b1a27e63
Green3 <afik.bracha@yahoo.com>
2013-12-23 23:24:39 +0200

    Update sh_ammo_smg1.lua

--- schema/items/sh_ammo_smg1.lua
+++ schema/items/sh_ammo_smg1.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -8,6 +8,7 @@ local ITEM = Clockwork.item:New("ammo_base");
 	ITEM.cost = 210;
 	ITEM.model = "models/items/boxmrounds.mdl";
 	ITEM.weight = 2;
+	ITEM.access = "V";
 	ITEM.uniqueID = "ammo_smg1";
 	ITEM.business = true;
 	ITEM.ammoClass = "smg1";
