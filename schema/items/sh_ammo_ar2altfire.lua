04d3a66ab96ffdb11f72a604de9d5715a5bd68da
Green3 <afik.bracha@yahoo.com>
2013-12-23 23:23:03 +0200

    Update sh_ammo_ar2altfire.lua

--- schema/items/sh_ammo_ar2altfire.lua
+++ schema/items/sh_ammo_ar2altfire.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -8,6 +8,7 @@ local ITEM = Clockwork.item:New("ammo_base");
 	ITEM.cost = 240;
 	ITEM.model = "models/items/combine_rifle_ammo01.mdl";
 	ITEM.weight = 1;
+	ITEM.access = V;
 	ITEM.business = true;
 	ITEM.ammoClass = "ar2altfire";
 	ITEM.ammoAmount = 90;
