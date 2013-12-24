4625aa4d34878286ed3f36ca610037dea3e26819
Green3 <afik.bracha@yahoo.com>
2013-12-24 13:57:43 +0200

    Update sh_beer.lua

--- schema/items/sh_beer.lua
+++ schema/items/sh_beer.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -8,6 +8,7 @@ ITEM.name = "Beer";
 ITEM.cost = 30;
 ITEM.model = "models/props_junk/garbage_glassbottle003a.mdl";
 ITEM.weight = 0.6;
+ITEM.access = "v";
 ITEM.business = true;
 ITEM.attributes = {Strength = 10};
 ITEM.description = "A glass bottle filled with liquid, it has a funny smell.";
