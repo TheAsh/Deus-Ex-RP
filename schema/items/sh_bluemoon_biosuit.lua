39b96e5ff0413e06f8e884c4b4ec619ee159445b
Green3 <afik.bracha@yahoo.com>
2013-12-24 13:59:14 +0200

    Update sh_bluemoon_biosuit.lua

--- schema/items/sh_bluemoon_biosuit.lua
+++ schema/items/sh_bluemoon_biosuit.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -7,6 +7,7 @@ local ITEM = Clockwork.item:New("custom_clothes");
 	ITEM.cost = 2750;
 	ITEM.name = "Bluemoon Biosuit";
 	ITEM.weight = 2;
+	ITEM.access = "m";
 	ITEM.business = true;
 	ITEM.armorScale = 0.225;
 	ITEM.replacement = "models/bio_suit/slow_bio_suit.mdl";
