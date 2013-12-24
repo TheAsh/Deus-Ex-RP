8433faee8211bf807c905700c02fcfc59e8fd08b
Green3 <afik.bracha@yahoo.com>
2013-12-24 14:08:30 +0200

    Update sh_combat_uniform.lua

--- schema/items/sh_combat_uniform.lua
+++ schema/items/sh_combat_uniform.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -8,6 +8,7 @@ local ITEM = Clockwork.item:New("custom_clothes");
 	ITEM.name = "Combat Uniform";
 	ITEM.group = "group03";
 	ITEM.weight = 1;
+	ITEM.access = "m";
 	ITEM.business = true;
 	ITEM.armorScale = 0.1;
 	ITEM.description = "A combat uniform with a yellow insignia.\nProvides you with 10% bullet resistance.";
