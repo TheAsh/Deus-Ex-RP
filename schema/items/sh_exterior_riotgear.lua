d8f9ea983a03820883d9705c2d0adae3e90c5fa4
Green3 <afik.bracha@yahoo.com>
2013-12-24 14:17:27 +0200

    Update sh_exterior_riotgear.lua

--- schema/items/sh_exterior_riotgear.lua
+++ schema/items/sh_exterior_riotgear.lua
@@ -1,5 +1,5 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	ï¿½ 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
@@ -7,6 +7,7 @@ local ITEM = Clockwork.item:New("custom_clothes");
 	ITEM.cost = 4000;
 	ITEM.name = "Exterior Riotgear";
 	ITEM.weight = 2.5;
+	ITEM.access = "m";
 	ITEM.business = true;
 	ITEM.armorScale = 0.3;
 	ITEM.replacement = "models/riot_ex2.mdl";
